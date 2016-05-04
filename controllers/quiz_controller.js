b063adc280f0652345b495f6f780d0d16214a1e7
AdrianBarbero <adrianbarbero1994@gmail.com>
2016-05-04 12:32:09 +0200

    Tema Base de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,5 @@
 // GET /question
+/*
 exports.question =function(req,res,next){
 	var answer =req.query.answer || '';
 	var par =req.query.parametro || '';
@@ -32,5 +33,45 @@ exports.question =function(req,res,next){
   }
     
   };
-  
+  */
  
+var models = require('../models');
+//GET /quizzes
+exports.index= function(req,res,next){
+models.Quiz.findAll().then(function(quizzes){
+res.render('quizzes/index.ejs',{quizzes:quizzes});
+}).catch(function(error){next(error);});
+};
+//GET /quizzes/:id
+exports.show=function(req,res,next){
+models.Quiz.findById(req.params.quizId).then(function(quiz){ // Busca la primera pregunta
+if(quiz){
+var answer = req.query.answer || '';
+res.render('quizzes/show',{quiz: quiz, answer:answer});
+}
+else{
+throw new Error('No existe ese quiz en la BBDD.');
+}
+}).catch(function(error){next(error);});
+};
+//GET /quizzes/:id/check
+exports.check = function(req,res,next){
+models.Quiz.findById(req.params.quizId).then(function(quiz){ //Busca la primera pregunta
+if(quiz){
+var answer = req.query.answer ||"";
+var result = answer ===quiz.answer ? 'Correcta' : 'Incorrecta';
+res.render('quizzes/result',{result :result, answer: answer});
+}
+else{
+throw new Error('No existe ese quiz en la BBDD.');
+}
+}).catch(function(error){next(error);});
+};
+//GET /quizzes
+/*exports.search=function(res,req,next){
+models.Quiz.findAll({where :["quiz.question like ?", search]}).then(function(quizzes){
+var search = req.query.search || "";
+res.render('/quizzes',{search :search});
+res.render('/quizzes/search',{pregunta : quiz.question});
+}).catch(function(error){next(error);});
+}*/
