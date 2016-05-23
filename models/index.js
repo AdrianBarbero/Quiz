b73b677f965e0991b201a9c9edb58d4beb2afad2
AdrianBarbero <adrianbarbero1994@gmail.com>
2016-05-23 17:42:08 +0200

    Usuarios

--- models/index.js
+++ models/index.js
@@ -8,21 +8,28 @@ var Sequelize = require('sequelize');
 // DATABASE_STORAGE =quiz.sqlite
 //Usar BBDD Postgres:
 // DATABASE_URL =postgres://user:passwd@host:port/database
-var url, storage;
-
-if(!process.env.DATABASE_URL){
-	url ="sqlite:///";
-	storage="quiz.sqlite";
-}else{
-	url = process.env.DATABASE_URL;
-	storage = process.env.DATABASE_STORAGE || "";
-}
-
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)\:(.*)\/(.*)/);
+var DATABASE_PROTOCOL = url[1];
+var DATABASE_DIALECT  = url[1];
+var DATABASE_USER     = url[2];
+var DATABASE_PASSWORD = url[3];
+var DATABASE_HOST     = url[4];
+var DATABASE_PORT     = url[5];
+var DATABASE_NAME     = url[6];
+var DATABASE_STORAGE  =process.env.DATABASE_STORAGE;
 
 //Usar BBDD SQLite: 
-var sequelize = new Sequelize(url,
-	                           {storage: storage,
-	                            omitNull: true});
+var sequelize = new Sequelize(
+	 DATABASE_NAME,
+	 DATABASE_USER,
+	 DATABASE_PASSWORD,
+	 { dialect: DATABASE_DIALECT,
+	   protocol: DATABASE_PROTOCOL,
+	   port:     DATABASE_PORT,
+	   host: DATABASE_HOST,
+	   storage: DATABASE_STORAGE,
+	   omitNull: true
+	});
 
 //Importar la definición de la tabla Quiz de quiz.js
 var Quiz =sequelize.import(path.join(__dirname,'quiz'));
