c84a01d30e32f3d92261ae66d27a33cc105ed966
AdrianBarbero <adrianbarbero1994@gmail.com>
2016-05-26 17:23:33 +0200

    Tema 24

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/author',function(req,res,next){
 // Autoload de rutas que usen quizId
 router.param('quizId', quizController.load); // autoload :quizId
 router.param('userId', userController.load); // autoload :userId
-
+router.param('commentId', comment_controller); // autoload :commentId
 //Definición de rutas de sesion
 router.get('/session',   sessionController.new); //formulario login
 router.post('/session', sessionController.create); //crear sesion
@@ -43,7 +43,8 @@ router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, quizC
 router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+//Definición de rutas de comentarios.
 router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired,  commentController.create);
-
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, commentController.accept);
 module.exports = router;
